3b14f54cfe98323374750eaffc44a91a4042a4ae
Lucaszabalare <nellymarac@gmail.com>
2024-11-30 11:28:42 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Create a Dictionary
+cats = {"Ginger" : "Susie","Black" : "Bob", "White" : "Joe"}
+print(cats)
+
+# Accessing values from a dictionary
+my_cat = cats["Ginger"]
+print(f"My favourite cat is {my_cat}!")
+
+# Get the list of Keys
+list_keys = list(cats.keys())
+print(list_keys)
+
+# Get the list of Values
+list_values = list(cats.values())
+print(list_values)
